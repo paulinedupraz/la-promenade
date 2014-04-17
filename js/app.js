64cd1149acd2d00f7e94e4b2994296f5eb0847a4
Pauline Dupraz <durpazpauline@gmail.com>
2014-04-17 23:15:16 +0200

    init

---
+++ js/app.js
@@ -0,0 +1,54 @@
+// Change the hash to #! instead of #
+(function() {
+
+var get = Ember.get, set = Ember.set;
+
+Ember.Location.registerImplementation('hashbang', Ember.HashLocation.extend({ 
+
+    getURL: function() {
+        return get(this, 'location').hash.substr(2);
+    },
+
+    setURL: function(path) {
+        get(this, 'location').hash = "!"+path;
+        set(this, 'lastSetURL', "!"+path);
+    },
+
+    onUpdateURL: function(callback) {
+        var self = this;
+        var guid = Ember.guidFor(this);
+
+        Ember.$(window).bind('hashchange.ember-location-'+guid, function() {
+                Ember.run(function() {
+                    var path = location.hash.substr(2);
+                    if (get(self, 'lastSetURL') === path) { return; }
+
+                    set(self, 'lastSetURL', null);
+
+                    callback(location.hash.substr(2));
+                });
+        });
+    },
+
+    formatURL: function(url) {
+        return '#!'+url;
+    }
+
+}));
+
+})();
+
+App = Ember.Application.create();
+
+App.Router.reopen({
+    location: 'hashbang'
+})
+
+
+App.Router.map(function() {
+  
+
+});
+
+
+
